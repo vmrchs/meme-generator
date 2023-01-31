84d456ed7b6acfd009603e9234dccd2e4c66a55c
vmrchs <vmrchs@gmail.com>
2023-01-31 09:19:07 -0300

    BUG: fails to import image link from memesData

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    console.log(randomNumber);
+    console.log(memesArray);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
     <main>
       <form action="" className="form">
         <input type="text" className="form--input" placeholder="Top text" />
         <input type="text" className="form--input" placeholder="Bottom text" />
-        <button className="form--button">Get a new meme image 🖼️ </button>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼️{" "}
+        </button>
       </form>
+      <img src={memeImage} />
     </main>
   );
 }
