a7d36e640f62a4f25ec4ae48894d6c05160b55f4
vmrchs <vmrchs@gmail.com>
2023-02-01 16:02:21 -0300

    FEAT: meme image passed through props, using state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,26 +2,40 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState("");
+  //   const [memeImage, setMemeImage] = React.useState(
+  //     "http://i.imgflip.com/lbij.jpg"
+  //   );
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "",
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     console.log(randomNumber);
     console.log(memesArray);
-    setMemeImage(memesArray[randomNumber].url);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   }
 
   return (
     <main>
-      <form action="" className="form">
+      <div action="" className="form">
         <input type="text" className="form--input" placeholder="Top text" />
         <input type="text" className="form--input" placeholder="Bottom text" />
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼️{" "}
         </button>
-      </form>
-      <img src={memeImage} />
+      </div>
+      <img src={meme.randomImage} className="meme--image" />
     </main>
   );
 }
