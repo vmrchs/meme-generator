054c2139a500f0e86ce4281d922a2716dc8a2cc3
vmrchs <vmrchs@gmail.com>
2023-02-10 16:25:07 -0300

    working some more on the meme generator

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -26,16 +26,44 @@ export default function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <main>
       <div action="" className="form">
-        <input type="text" className="form--input" placeholder="Top text" />
-        <input type="text" className="form--input" placeholder="Bottom text" />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Top text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼️{" "}
         </button>
       </div>
-      <img src={meme.randomImage} className="meme--image" />
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">Top text</h2>
+        <h2 className="meme--text bottom">Bottom text</h2>
+      </div>
     </main>
   );
 }
